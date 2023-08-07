f5df6d634466e8b6bb32f95d5754506311821be8
gleison <gleisondeivison3@gmail.com>
2023-08-07 19:58:59 -0300

    estilizando a sidebar

---
+++ src/components/Avatar/index.tsx
@@ -0,0 +1,5 @@
+import { FotoPerfil } from './styles'
+
+const Avatar = () => <FotoPerfil src="https://github.com/Gleison1991.png" />
+
+export default Avatar
--- src/containers/Sidebar/index.tsx
+++ src/containers/Sidebar/index.tsx
@@ -1,9 +1,22 @@
+import Avatar from '../../components/Avatar'
+import Paragrafo from '../../components/Paragrafo'
 import Titulo from '../../components/Titulo'
+
+import { Descricao, BotaoTema, SidebarContainer } from './styles'
 
 const Sidebar = () => (
   <aside>
-    <img src="https://github.com/Gleison1991.png" />
-    <Titulo fontSize={20}>Gleison 1991</Titulo>
+    <SidebarContainer>
+      <Avatar />
+      <Titulo fontSize={20}>Gleison 1991</Titulo>
+      <Paragrafo tipo="secundario" fontSize={16}>
+        Gleison1991
+      </Paragrafo>
+      <Descricao tipo="principal" fontSize={12}>
+        Dev Front-End
+      </Descricao>
+      <BotaoTema>Trocar tema</BotaoTema>
+    </SidebarContainer>
   </aside>
 )
 
